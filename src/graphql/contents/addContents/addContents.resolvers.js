bb9b60658071aa8b8e4be2409816b81d1ccde3dc
minki-afun <minki@a-fun.co.kr>
2021-11-04 14:25:28 +0900

    commit

--- src/graphql/contents/addContents/addContents.resolvers.js
+++ src/graphql/contents/addContents/addContents.resolvers.js
@@ -1,26 +1,29 @@
 import client from "../../client"
+import { protect } from "../../graphqlUtils"
 
 
 export default {
   Mutation: {
-    addContents: async(
+    addContents: protect(async(
       _,
-      {product, price, text}
+      {product, price, text, date, sign},
+      {loggedInUser}
       ) => {
+        console.log(loggedInUser);
         await client.content.create({
         data:{
           product,
           price,
           text,
-          sign: true,
-          date: 2020,
-          userId: 1
+          sign,
+          date,
+          userId: loggedInUser.userId
         }
       })
       return{
         result: false,
         error: "Can't add to account-book"
       }
-    }
+    })
   }
 }
