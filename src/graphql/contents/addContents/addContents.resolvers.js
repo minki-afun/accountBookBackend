ce061975d38242c64894beb2cc2d1ea600ace0e8
minki-afun <minki@a-fun.co.kr>
2021-11-12 13:39:29 +0900

    backend

--- src/graphql/contents/addContents/addContents.resolvers.js
+++ src/graphql/contents/addContents/addContents.resolvers.js
@@ -1,45 +1,38 @@
 import client from "../../client"
 import { protect } from "../../graphqlUtils"
 
-
 export default {
   Mutation: {
     addContents: protect(
-    async(
-      _,
-      {product, price, text, date},
-      {loggedInUser}
-      ) => {
-        console.log(loggedInUser.id)
+      async (_, { product, price, text, date }, { loggedInUser }) => {
         try {
-          if(product !== "" && price !== 0 && date !== 0){
+          if (product !== "" && price !== 0 && date !== 0) {
             const checkBeforeAdd = await client.content.create({
-              data:{
+              data: {
                 product,
                 price,
                 text,
                 date,
-                sign:true,
+                sign: true,
                 userId: loggedInUser.id,
-              }
+              },
             })
             console.log(checkBeforeAdd)
-            
-            return{
-              result:true,
-              error:"등록이 완료되었습니다."
+
+            return {
+              result: true,
+              error: "등록이 완료되었습니다.",
             }
           }
-          
         } catch (error) {
           console.log(error)
         }
-        
-      return{
-        result:false,
-        error: "값을 제대로 입력해주세요."
+
+        return {
+          result: false,
+          error: "값을 제대로 입력해주세요.",
+        }
       }
-    }
-    )
-  }
+    ),
+  },
 }
--- src/graphql/graphqlUtils.js
+++ src/graphql/graphqlUtils.js
@@ -3,36 +3,37 @@ import client from "./client"
 
 // JWT토큰이 있다면 아이디를 추출해 유저 정보를 가져오기
 export const getUser = async (token) => {
-    try {
-        if(!token) {
-            return null
-        }
-        const { id } = await jwt.verify(token, process.env.SECRET_KEY)
-        const user = await client.user.findUnique({ where: { id }})
-        if (user) {
-            return user
-        } else {
-            return null
-        }
-    } catch {
-        return null
+  try {
+    if (!token) {
+      return null
     }
+    const { id } = await jwt.verify(token, process.env.SECRET_KEY)
+    const user = await client.user.findUnique({ where: { id } })
+    if (user) {
+      return user
+    } else {
+      return null
+    }
+  } catch {
+    return null
+  }
 }
 
 // 로그인한 상태여야지 그다음 함수 사용가능한 함수
 export function protect(ourResolver) {
-    return function (root, args, context, info) {
-        if (!context.loggedInUser) {
-            const query = info.operation.operation === "query"
-            if (query) {
-                return null
-            } else {
-                return { 
-                    result: false,
-                    error: "Please login."
-                }
-            }
+  return function (root, args, context, info) {
+    if (!context.loggedInUser) {
+      const query = info.operation.operation === "query"
+      if (query) {
+        return null
+      } else {
+        return {
+          result: false,
+          error: "Please login.",
         }
-        return ourResolver(root, args, context, info)
+      }
     }
+    console.log("tlqkf")
+    return ourResolver(root, args, context, info)
+  }
 }
